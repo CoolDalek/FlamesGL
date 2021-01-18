08a1a67f2a7e16afaf1c268e2fe947e72bc52846
Volodymyr Lahush <vladumurlr@gmail.com>
2021-01-18 15:24:40 +0200

    Complete refactor of mvc package.

--- src/main/scala/engine/mvc/Model.scala
+++ src/main/scala/engine/mvc/Model.scala
@@ -2,6 +2,17 @@ package engine.mvc
 
 trait Model[State] {
 
-  def state: State
+  def apply(): State = state()
+
+  def state(): State
+
+}
+
+object Model {
+
+  def model[State](modelState: State): Model[State] =
+    new Model[State] {
+      override val state: State = modelState
+    }
 
 }
