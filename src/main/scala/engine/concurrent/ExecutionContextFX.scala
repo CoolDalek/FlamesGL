b60a7bf11aab3d4e712ac400a7bf03f6fba842a7
Volodymyr Lahush <vladumurlr@gmail.com>
2021-01-20 21:06:01 +0200

    Some little improvements.

--- src/main/scala/engine/concurrent/ExecutionContextFX.scala
+++ src/main/scala/engine/concurrent/ExecutionContextFX.scala
@@ -1,10 +1,13 @@
 package engine.concurrent
 
+import engine.utils.Self.InvariantSelf
 import scalafx.application.Platform
 
 import scala.concurrent.ExecutionContext
 
-object ExecutionContextFX extends ExecutionContext {
+object ExecutionContextFX
+  extends ExecutionContext
+    with InvariantSelf[ExecutionContextFX.type] {
 
   override def execute(runnable: Runnable): Unit =
     Platform.runLater(runnable)
